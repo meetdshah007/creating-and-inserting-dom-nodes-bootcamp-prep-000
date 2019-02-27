e338cc9c2758b54fef75d62f77ba34a3f4ecef8c
Meet Shah <meetdshah007@gmail.com>
2019-02-27 11:08:37 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+const el = document.createElement(div);
+el.innerHTML = "Hello, DOM!";
+el.style.backgroundColor = "#f9f9f9";
+
+document.body.appendChild(el);
